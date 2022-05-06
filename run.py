26369962baa280ce130fe0c4bba76ada35b5dd77
MarkHewitt76 <markyhewitt@gmail.com>
2022-05-06 14:48:00 +0000

    Create main_user_interface funtion to get input string from user

--- run.py
+++ run.py
@@ -12,6 +12,27 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('kayleighs-cakes-mh76')
 
+
+def main_user_interface():
+    """
+    A list of options presented to the user. 
+    Number input (1-6) determines the action taken.
+    """
+    print("What would you like to do?\n")
+    print("1. View existing products")
+    print("2. View potential new products")
+    print("3. View all products")
+    print("4. View most popular products\n   (based on customer survey)")
+    # print("5. View most popular new products\n   (based on customer survey)")
+    print("5. Add products to 'new menu short list' worksheet")
+    print("6. Exit the program\n")
+    
+    option = input("Enter your choice here (1-6):\n")
+    print(f"You selected {option}")
+
+
+main_user_interface()
+
 current_products = SHEET.worksheet("current products")
 data = current_products.get_all_values()
 print(data)
