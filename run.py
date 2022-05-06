a3714c9b6ffcb3b1ff976d9f650a8bd92fbf38f4
MarkHewitt76 <markyhewitt@gmail.com>
2022-05-06 15:46:19 +0000

    Add validation to check user input and request loop until input is valid

--- run.py
+++ run.py
@@ -18,20 +18,40 @@ def main_user_interface():
     A list of options presented to the user. 
     Number input (1-6) determines the action taken.
     """
-    print("What would you like to do?\n")
-    print("1. View existing products")
-    print("2. View potential new products")
-    print("3. View all products")
-    print("4. View most popular products\n   (based on customer survey)")
-    # print("5. View most popular new products\n   (based on customer survey)")
-    print("5. Add products to 'new menu short list' worksheet")
-    print("6. Exit the program\n")
+    while True:
+        print("What would you like to do?\n")
+        print("1. View existing products")
+        print("2. View potential new products")
+        print("3. View all products")
+        print("4. View most popular products\n   (based on customer survey)")
+        print("5. Add products to 'new menu short list' worksheet")
+        print("6. Exit the program\n")
     
-    option = input("Enter your choice here (1-6):\n")
-    print(f"You selected {option}")
+        option = input("Enter your choice here (1-6):\n")
+
+        if validate_input(option):
+            print("Input valid!")
+            print("Working...\n")
+            break
+
+    return option
 
 
-main_user_interface()
+def validate_input(option):
+    """
+    Uses a try statement to raise an error if the user's
+    input is not a number between 1 and 6.
+    """
+    try:
+        int(option) in range(1, 6)
+    except ValueError:
+        print("Invalid input! Please enter a number between 1 and 6.\n")
+        return False
+
+    return True
+
+
+option = main_user_interface()
 
 current_products = SHEET.worksheet("current products")
 data = current_products.get_all_values()
